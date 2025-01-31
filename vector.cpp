e22049421f5e41f408480e53be820a13f8eb871f
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-01-31 16:37:19 -0500

    constructor reserve and destructor

--- vector.cpp
+++ vector.cpp
@@ -1,22 +1,78 @@
 #include <initializer_list>
 #include <memory>
 
-template <class T, class Allocator = std::allocator<T>> class vector
+template <class T, class Allocator = std::allocator<T>>
+class Vector
 {
 private:
-    size_t m_size{};
-    size_t m_capacity{};
-    T*     m_data{ nullptr };
+    size_t    m_size{};
+    size_t    m_capacity{};
+    T*        m_data{ nullptr };
+    Allocator m_allocator{};
+
+    void deallocate()
+    {
+        using Traits = std::allocator_traits<Allocator>;
+        for (size_t i = 0; i < m_size; ++i)
+        {
+            Traits::destroy(m_allocator, m_data + i);
+        }
+        Traits::deallocate(m_allocator, m_data, m_capacity);
+    }
 
 public:
-    Vector(size_t size, const T& value);
+    explicit Vector(size_t size, const T& value);
     Vector(const std::initializer_list<T> values);
-    void                          reserve(size_t capacity);
-    void                          push_back(const T& value);
-    template <class... Args> void emplace_back(Args&&... args);
-    void                          clear();
-    size_t                        size() const { return m_size; }
-    size_t                        capacity() const { return m_capacity; }
-    Allocator                     get_allocator() const { return {}; }
-    T*                            data() { return m_data; }
+    void reserve(size_t capacity);
+    void push_back(const T& value);
+    template <class... Args>
+    void      emplace_back(Args&&... args);
+    void      clear();
+    size_t    size() const { return m_size; }
+    size_t    capacity() const { return m_capacity; }
+    T*        data() { return m_data; }
+    Allocator get_allocator() const { return m_allocator; }
+    ~Vector();
 };
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::Vector(size_t size, const T& value)
+{
+    reserve(size);
+
+    for (size_t i{}; i < size; ++i)
+        push_back(value);
+}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::Vector(const std::initializer_list<T> values)
+{
+    reserve(values.size());
+
+    for (auto&& value : values)
+        push_back(value);
+}
+
+template <typename T, typename Allocator>
+void Vector<T, Allocator>::reserve(size_t capacity)
+{
+    using Traits = std::allocator_traits<Allocator>;
+    if (capacity <= m_capacity)
+        return;
+
+    T* newData = Traits::allocate(m_allocator, capacity);
+
+    for (size_t i{ 0 }; i < m_size; ++i)
+        Traits::construct(m_allocator, newData + i, m_data[i]);
+
+    deallocate();
+
+    m_data     = newData;
+    m_capacity = capacity;
+}
+
+template <typename T, typename Allocator>
+Vector<T, Allocator>::~Vector()
+{
+    deallocate();
+}
