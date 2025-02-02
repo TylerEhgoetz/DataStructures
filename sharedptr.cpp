7d11e72598e3b6f19c4db8b1c0605d842b5f8c3e
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-02-02 13:35:34 -0500

    big 5 declarations

--- sharedptr.cpp
+++ sharedptr.cpp
@@ -10,6 +10,20 @@ struct ControlBlock
 template <typename T>
 class SharedPointer
 {
+public:
+    SharedPointer()
+        : SharedPointer(nullptr)
+    {}
+    SharedPointer(std::nullptr_t) {}
+    explicit SharedPointer(T* ptr)
+        : m_ptr{ ptr }, m_controlBlock{ new ControlBlock{ 1 } }
+    {}
+
+    SharedPointer(const SharedPointer& other);
+    SharedPointer& operator=(const SharedPointer& other);
+    SharedPointer(SharedPointer&& other) noexcept;
+    SharedPointer& operator=(SharedPointer&& other) noexcept;
+    ~SharedPointer();
 
 private:
     T*            m_ptr{ nullptr };
