50b648f3c1f7b510e721786f28763b94ec70b14a
Tyler Ehgoetz <tyler.ehgoetz@gmail.com>
2025-02-06 15:18:53 -0500

    produce and consume

--- ProducerConsumer.cpp
+++ ProducerConsumer.cpp
@@ -1,7 +1,15 @@
+#include <atomic>
 #include <condition_variable>
 #include <mutex>
 #include <queue>
 #include <thread>
+
+template <typename T>
+struct Data
+{
+    T    data{};
+    bool processed{ false };
+};
 
 template <typename T>
 class ProducerConsumer
@@ -13,17 +21,54 @@ public:
     {}
 
 private:
-    void Produce();
-    void Consume();
-    void DoSomething(const T& item)
+    void Produce()
     {
-        // do something
+        while (!m_done)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
+            {
+                std::lock_guard lock{ m_mutex };
+                Data<T>         data;
+                data.data = GenerateData();
+                m_queue.push(data);
+            }
+            m_condition.notify_one();
+        }
     }
+
+    void Consume()
+    {
+        while (!m_done)
+        {
+            std::unique_lock lock{ m_mutex };
+            m_condition
+                .wait(lock, [this] { return !m_queue.empty() || m_done; });
+            if (m_done && m_queue.empty())
+            {
+                break;
+            }
+            auto item = m_queue.front();
+            m_queue.pop();
+            lock.unlock();
+            ProcessData(item);
+            if (item.processed)
+            {
+                break;
+            }
+        }
+    }
+
+    void ProcessData(Data<T>& data)
+    {
+        // Process data
+    }
+
     T GenerateData() { return T{}; }
 
     mutable std::mutex      m_mutex{};
-    std::queue<T>           m_queue{};
+    std::queue<Data<T>>     m_queue{};
     std::condition_variable m_condition{};
     std::thread             m_producer{};
     std::thread             m_consumer{};
+    std::atomic<bool>       m_done{ false };
 };
